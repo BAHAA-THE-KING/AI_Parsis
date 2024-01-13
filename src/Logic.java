ed502c2a218ee150ce0e30a0f4003600d5171833
abdulrahman-kharzoum <aboodkh920@gmail.com>
2024-01-13 13:27:21 +0300

    clean main! :)

--- src/Logic.java
+++ src/Logic.java
@@ -9,35 +9,19 @@ public class Logic {
         Node root = new Node(null, initialBoard);
         Structure.print(initialBoard);
         humanTurn(root);
-
-
-        System.out.println("Initial...");
-        Structure.print(initialBoard);
-        List<Move> moves = new ArrayList<>();
-        moves.add(new Move(10,"ten",1));
-        moves.add(new Move(1,"khal",1));
-        List<Node> nextStates = Structure.getNextStates(root,moves,'c');
-        for(Node node:nextStates){
-            System.out.println("--------------------------------------------------");
-            Structure.print(node.board);
-        }
     }
 
-    static Scanner scanner=new Scanner(System.in);
+    static Scanner scanner = new Scanner(System.in);
 
     public static void humanTurn(Node node) {
         System.out.println("It's your turn");
         List<Move> humanMoves = Structure.throwShells();
-//        List<Move> humanMoves = new ArrayList<>();
-//        humanMoves.add(new Move(2 , "dua" , 1));
-//        humanMoves.add(new Move(3 , "three" , 1));
-
-        while (!humanMoves.isEmpty()){
+        while (!humanMoves.isEmpty()) {
 
             //Choosing a move
             System.out.println("Yor moves :");
             int k = 1;
-            for(Move mv : humanMoves){
+            for (Move mv : humanMoves) {
                 System.out.println(k + " - [" + mv + "]");
                 k++;
             }
@@ -50,29 +34,30 @@ public class Logic {
             //checking valid pieces
             List<Integer> validPieces = new ArrayList<>();
 
-            for(int i = 0 ; i < node.board.piecesHuman.length ; i++){
-                if(Structure.canMove(node.board , 'h' , i, humanSelectedMove)){
-                    validPieces.add(i) ;
+            for (int i = 0; i < node.board.piecesHuman.length; i++) {
+                if (Structure.canMove(node.board, 'h', i, humanSelectedMove)) {
+                    validPieces.add(i);
                 }
             }
 
             //if no valid pieces
-            if(validPieces.isEmpty()){
+            if (validPieces.isEmpty()) {
                 System.out.println("you can't move any piece with the selected move");
                 continue;
-            };
+            }
+            ;
 
             //choosing a piece
-            System.out.println("The pieces you can move with [ "+humanSelectedMove+" ] :");
+            System.out.println("The pieces you can move with [ " + humanSelectedMove + " ] :");
             k = 1;
-            for(Integer piece : validPieces){
+            for (Integer piece : validPieces) {
                 System.out.println(k + " - [" + Structure.getSymbol('h', piece) + "]");
                 k++;
             }
             System.out.print("Choose a piece to move : ");
 
             int selectedPiece = scanner.nextInt() - 1;
-            Structure.applyMove(node.board , validPieces.get(selectedPiece) , humanSelectedMove , 'h');
+            Structure.applyMove(node.board, validPieces.get(selectedPiece), humanSelectedMove, 'h');
 
             Structure.print(node.board);
 
@@ -85,6 +70,7 @@ public class Logic {
     public static void computerTurn(Node node) {
         List<Move> moves = Structure.throwShells();
         Node nextNode = new MaximizingNode(node, node.board, moves).getMaxEvaluation().key;
+        System.out.println("Computer have moves:" + moves);
         Structure.print(nextNode.board);
         humanTurn(nextNode);
     }
